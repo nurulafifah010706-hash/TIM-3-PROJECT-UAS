096b5fbc6ccbceadc1771d9909e455367563a494
nurulafifah010706-hash <nurulafifah010706-hash>
2026-04-16 22:14:37 +0700

    main

--- main.c
+++ main.c
@@ -1,66 +1,41 @@
 #include <stdio.h>
-<<<<<<< HEAD
-#include "alat/alat.h"
-=======
-#include "alat.h"
->>>>>>> 608e2391435f6e1bc4cf49f0cd3841d9b1578604
+#include "header.h"
 
 int main() {
-    int pilih;
+    int pilihan;
+    int a, b;
 
     do {
-<<<<<<< HEAD
-        printf("\n=== MENU ALAT ===\n");
-=======
-        printf("\n=== MENU ADMIN ===\n");
->>>>>>> 608e2391435f6e1bc4cf49f0cd3841d9b1578604
-        printf("1. Tambah Alat\n");
-        printf("2. Edit Alat\n");
-        printf("3. Hapus Alat\n");
-        printf("4. Lihat Alat\n");
-<<<<<<< HEAD
-        printf("0. Keluar\n");
-        printf("Pilih: ");
-        scanf("%d", &pilih);
+        tampilkanMenu();
+        printf("Pilih menu: ");
+        scanf("%d", &pilihan);
 
-        switch(pilih) {
-            case 1: tambahAlat(); break;
-            case 2: editAlat(); break;
-            case 3: hapusAlat(); break;
-            case 4: lihatAlat(); break;
-            case 0: printf("Keluar...\n"); break;
-            default: printf("Pilihan tidak valid!\n");
+        switch (pilihan) {
+            case 1:
+                printf("Masukkan angka 1: ");
+                scanf("%d", &a);
+                printf("Masukkan angka 2: ");
+                scanf("%d", &b);
+                printf("Hasil: %d\n", tambah(a, b));
+                break;
+
+            case 2:
+                printf("Masukkan angka 1: ");
+                scanf("%d", &a);
+                printf("Masukkan angka 2: ");
+                scanf("%d", &b);
+                printf("Hasil: %d\n", kurang(a, b));
+                break;
+
+            case 3:
+                printf("Program selesai.\n");
+                break;
+
+            default:
+                printf("Pilihan tidak valid!\n");
         }
-    } while(pilih != 0);
+
+    } while (pilihan != 3);
 
     return 0;
 }
-=======
-        printf("5. Keluar\n");
-        scanf("%d", &pilih);
-
-       switch(pilih) {
-    case 1:
-        tambahAlat();
-        break;
-    case 2:
-        editAlat();
-        break;
-    case 3:
-        hapusAlat();
-        break;
-    case 4:
-        lihatAlat();
-        break;
-    case 5:
-        printf("Keluar...\n");
-        break;
-    default:
-        printf("Pilihan tidak valid!\n");
-}
-
-    } while(pilih != 4);
-
-    return 0;
-}
->>>>>>> 608e2391435f6e1bc4cf49f0cd3841d9b1578604
