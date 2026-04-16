a5e4f9712f6e4f3d572ea9b6158651ec16dec0fe
wafinafis07 <wafinafis07@gmail.com>
2026-04-16 13:03:04 +0700

    update alat

--- main.c
+++ main.c
@@ -1,39 +1,28 @@
 #include <stdio.h>
-#include "alat.h"
+#include "alat/alat.h"
 
 int main() {
     int pilih;
 
     do {
-        printf("\n=== MENU ADMIN ===\n");
+        printf("\n=== MENU ALAT ===\n");
         printf("1. Tambah Alat\n");
         printf("2. Edit Alat\n");
         printf("3. Hapus Alat\n");
         printf("4. Lihat Alat\n");
-        printf("5. Keluar\n");
+        printf("0. Keluar\n");
+        printf("Pilih: ");
         scanf("%d", &pilih);
 
-       switch(pilih) {
-    case 1:
-        tambahAlat();
-        break;
-    case 2:
-        editAlat();
-        break;
-    case 3:
-        hapusAlat();
-        break;
-    case 4:
-        lihatAlat();
-        break;
-    case 5:
-        printf("Keluar...\n");
-        break;
-    default:
-        printf("Pilihan tidak valid!\n");
-}
-
-    } while(pilih != 4);
+        switch(pilih) {
+            case 1: tambahAlat(); break;
+            case 2: editAlat(); break;
+            case 3: hapusAlat(); break;
+            case 4: lihatAlat(); break;
+            case 0: printf("Keluar...\n"); break;
+            default: printf("Pilihan tidak valid!\n");
+        }
+    } while(pilih != 0);
 
     return 0;
 }
